5b7bef1d72f63ea12bfe4008f7b8109d98fa201a
msung99 <msung6924@naver.com>
2022-11-19 13:51:15 +0900

    Comment: 각 함수의 주석처리 변경

--- template.py
+++ template.py
@@ -18,13 +18,11 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import fetch_california_housing
 from sklearn.metrics import precision_score, recall_score
 
-# Load the csv file at the given path into the pandas DataFrame and return the DataFrame
 def load_dataset(dataset_path):
     data_df = pd.read_csv(dataset_path)
     return data_df
 
-# For the given DataFrame, return the following statistical analysis results in order
-# Number of features / Number of data for class 0 / Number of data for class 1
+
 def dataset_stat(dataset_df):
 	freq_zero = 0
 	freq_one = 0
@@ -35,62 +33,40 @@ def dataset_stat(dataset_df):
 			freq_one += 1
 	return (dataset_df.shape[1] -1 , freq_zero, freq_one)
 
-# Splitting the given DataFrame and return train data, test data, train label, and test label in order
-# You must split the data using the given test size
+
 def split_dataset(dataset_df, testset_size):
 	X = dataset_df.drop(columns = "target", axis = 1)
 	y = dataset_df["target"]
 	X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size = testset_size)
 	return (X_train, X_test, Y_train, Y_test)
 
-# Using the given train dataset, train the decision tree model
-# You can implement with "default arguments"
-# After the training, evaluate the performances of the model using the given test dataset
-# Return three performance metrics (accuracy, precision, recall) in order
+
 def decision_tree_train_test(x_train, x_test, y_train, y_test):
-	#dt_cls = DecisionTreeClassifier()
-	#dt_cls.fit(x_train, y_train) # 훈련 진행
 	pipe = make_pipeline(StandardScaler(), DecisionTreeClassifier())
 	pipe.fit(x_train, y_train)
 	acc = accuracy_score(pipe.predict(x_test), y_test) 
 	prec = precision_score(pipe.predict(x_test), y_test) 
 	rec = recall_score(pipe.predict(x_test), y_test)
-	#print(confusion_matrix(y_test, pipe.predict(x_test)))
 	return (acc, prec, rec)
-	#print(accuracy_score(pipe.predict(x_test), y_test))
-	#print(precision_score(pipe.predict(x_test), y_test))
-	#print(recall_score(pipe.predict(x_test), y_test))
-	#print(confusion_matrix(y_test, pipe.predict(x_test)))
 
-# Using the given train dataset, train the random forest model
-# You can implement with default arguments
-# After the training, evaluate the performances of the model using the given test dataset
-# Return three performance metrics (accuracy, precision, recall) in order
+
 def random_forest_train_test(x_train, x_test, y_train, y_test):
-	#dt_cls = RandomForestClassifier()
-	#dt_cls.fit(x_train, y_train) # 훈련 진행
 	pipe = make_pipeline(StandardScaler(), RandomForestClassifier())
 	pipe.fit(x_train, y_train)
 	acc = accuracy_score(pipe.predict(x_test), y_test) 
 	prec = precision_score(pipe.predict(x_test), y_test) 
 	rec = recall_score(pipe.predict(x_test), y_test)
-	#print(confusion_matrix(y_test, pipe.predict(x_test)))
 	return (acc, prec, rec)
 
-# Using the given train dataset, train the pipeline consists of a standard scaler and SVM
-# You can implement with default arguments
-# After the training, evaluate the performances of the model using the given test dataset
-# Return three performance metrics (accuracy, precision, recall) in order
+
 def svm_train_test(x_train, x_test, y_train, y_test):
-	#dt_cls = RandomForestClassifier()
-	#dt_cls.fit(x_train, y_train) # 훈련 진행
 	pipe = make_pipeline(StandardScaler(), SVC())
 	pipe.fit(x_train, y_train)
 	acc = accuracy_score(pipe.predict(x_test), y_test) 
 	prec = precision_score(pipe.predict(x_test), y_test) 
 	rec = recall_score(pipe.predict(x_test), y_test)
-	#print(confusion_matrix(y_test, pipe.predict(x_test)))
 	return (acc, prec, rec)
+
 
 def print_performances(acc, prec, recall):
 	#Do not modify this function!
@@ -103,11 +79,12 @@ if __name__ == '__main__':
 	#Do not modify the main script!
 	data_path = sys.argv[1]
 	data_df = load_dataset(data_path)
+
 	n_feats, n_class0, n_class1 = dataset_stat(data_df)
 	print ("Number of features: ", n_feats)
 	print ("Number of class 0 data entries: ", n_class0)
 	print ("Number of class 1 data entries: ", n_class1)
-	
+
 	print ("\nSplitting the dataset with the test size of ", float(sys.argv[2]))
 	x_train, x_test, y_train, y_test = split_dataset(data_df, float(sys.argv[2]))
 
@@ -122,5 +99,4 @@ if __name__ == '__main__':
 	acc, prec, recall = svm_train_test(x_train, x_test, y_train, y_test)
 	print ("\nSVM Performances")
 	print_performances(acc, prec, recall)
-
 	
